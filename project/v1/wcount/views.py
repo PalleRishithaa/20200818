549698b61afc81a8479e5a2c1ea1eb266b23b3e9
Rishithaa Palle <69782508+PalleRishithaa@users.noreply.github.com>
2020-08-19 21:06:20 +0530

    Update views.py

--- project/v1/wcount/views.py
+++ project/v1/wcount/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 import operator
 # Create your views here.
 def home(requests):
-    return HttpResponse('<h1>This is my page</h1><ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul>')
+    return HttpResponse('<h1>This is my page</h1><hr><ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul>')
 def aboutus(requests):
-    return HttpResponse('<h1>I am persuing B.E IT in vasavi college of engineering</h1><ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul>')
+    return HttpResponse('<h1>I am persuing B.E IT in vasavi college of engineering</h1><hr><ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul>')
 def myhobbies(requests):
-    return HttpResponse('<ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul><h1>MY Hobbies:</h1><ul><li>Watching TV</li><li>Painting</li></ul>')
+    return HttpResponse('<ul><li>Name:Rishithaa Palle</li><li>Roll.No: 1602-18-737-031</li></ul><hr><h1>MY Hobbies:</h1><ul><li>Watching TV</li><li>Painting</li></ul>')
 
 
